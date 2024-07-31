76635e220396c42d3d37e6e33757130f78082da7
Stefano <ste.martiaguirre@gmail.com>
2024-07-31 17:31:59 +0200

    primo commit

---
+++ src/main/java/com/generation/SpeedyBeans/SpeedyBeansApplication.java
@@ -0,0 +1,13 @@
+package com.generation.SpeedyBeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedyBeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedyBeansApplication.class, args);
+	}
+
+}
