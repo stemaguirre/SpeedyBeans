23d71afbb77bf27e179e7bb7f0623c35001a768d
Stefano <ste.martiaguirre@gmail.com>
2024-08-01 14:19:17 +0200

    piccole modifiche

--- src/main/java/com/generation/SpeedyBeans/entities/Persona.java
+++ src/main/java/com/generation/SpeedyBeans/entities/Persona.java
@@ -3,7 +3,7 @@ package com.generation.SpeedyBeans.entities;
 import lombok.Data;
 
 @Data
-public abstract class Persona extends Entity
+public abstract class Persona
 {
     int idPersona;
     String nome;
